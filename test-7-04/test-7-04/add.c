37bca4e17878a9abb507219ddac73b0ba3bf7f42
zyt6666666 <2982184092@qq.com>
2025-07-10 20:44:02 +0800

    初识C语言3

---
+++ test-7-04/test-7-04/add.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+//static int g_val = 2025; //全局变量
+
+
+
+static int Add(int x, int y) //静态函数
+{
+	return x + y; //返回a和b的和
+}
