8c753bb5099efaa5d47d3977d98f863cbfb74ed8
张云腾 <15234711471@163.com>
2025-07-22 22:11:33 +0800

    初阶C语言调试2

---
+++ test-7-22/test-7-22/test.c
@@ -0,0 +1,45 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//int Add(int a, int b)
+//{
+//	return a + b;
+//}
+//
+//int main()
+//{
+//	//调试
+//	int a = 10;
+//	int b = 20;
+//	int c = Add(a,b);
+//	printf("a + b = %d\n", c);
+//	return 0;
+//}
+
+//void test(int a[])
+//{
+//	
+//}
+//int main()
+//{
+//	int arr[10] = {1,2,3,4,5,6,7,8,9,10};
+//	test(arr);
+//	return 0;
+//}
+
+//int main()
+//{
+
+	//
+	//num = 20;// 这行代码会导致编译错误，因为num是一个常量
+	//int* p = (int*)&num; // 强制转换为int指针
+	//*p = 20; // 修改num的值
+	// 这种做法是不安全的，可能会导致未定义行为
+	//const int* cp = &num; // 正确的做法，cp是一个指向常量的指针
+	// *cp = 30; // 错误，不能修改常量的值
+
+	/*printf("num = %d\n", num);
+	return 0;
+}*/
